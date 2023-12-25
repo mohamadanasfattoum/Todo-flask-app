212104133ef91788a2dcd57b2684323e2382a330
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-25 17:30:28 +0100

    add def home

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+
+class Todo(db.Model): # database table
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean())
+
+
+
+
+
+@app.get('/')
 def home():
-    return 'Hello World, from Flask'
+    todo_tist = db.session.query(Todo).all()
+    return render_template('base.html', todo_list=todo_list)
+
+
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
